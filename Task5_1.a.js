cb12e1339e92dd14d0f80e9e0a0460bdc3114506
shalinisai9195 <110511957+shalinisai9195@users.noreply.github.com>
2022-08-05 15:21:12 +0530

    Add files via upload

---
+++ Task5_1.a.js
@@ -0,0 +1,18 @@
+// a.Print odd number in an array using anonymous function
+
+let ans = function () {
+
+    let oddnumber = [1,2,3,4,5,6,7,8,9,10]
+
+    for(let i=0; i< oddnumber.length;i++)
+    {
+        if(oddnumber[i] % 2 != 0)
+        {
+            console.log(oddnumber[i])
+        }
+    }
+};
+
+ans();
+
+
---
+++ Task5_1.b.js
@@ -0,0 +1,13 @@
+// b. Conver string array to title caps using anonymous function
+
+let result = function (name1,name2,name3,name4){
+    let namearray = [name1,name2,name3,name4]
+
+for(let i=0;i < namearray.length; i++){
+    console.log(namearray[i].toUpperCase())
+
+}
+
+};
+
+result("shakti","siva","ram","janu");
