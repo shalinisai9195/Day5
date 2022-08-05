a611049a920a60cfea4da3f53fe609007626aef2
shalinisai9195 <110511957+shalinisai9195@users.noreply.github.com>
2022-08-05 15:17:41 +0530

    Delete Task5_1.b.js

--- Task5_1.b.js
+++
@@ -1,8 +0,0 @@
-// b. Conver string array to title caps 
-
-let namearray = ["shakti","siva","ram","janu"]
-
-for(let i=0;i < namearray.length; i++){
-    console.log(namearray[i].toUpperCase())
-
-}
